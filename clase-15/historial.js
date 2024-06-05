ef83f41743b25dc6ab58cf82421589426c4ca87a
mariana.urzainqui <marianaurzainqui@gmail.com>
2024-06-05 11:34:39 -0300

    clase16

--- clase-15/historial.js
+++ clase-15/historial.js
@@ -25,3 +25,77 @@ const guardarEnHistorial = (objeto) =>{
     localStorage.setItem('historial', JSON.stringify(historial))
 }
 
+/* Cada elemento del historial tendra el siguiente formato
+{
+    accion: 'calculadora',
+    operacion: '+'||'-',
+    a: number,
+    b: number,
+    resultado: number,
+    fecha: dd/mm/aaaa || '18|01/2020'
+}
+
+Esto significa que cuando usemos guardarEnHistorial debemos guardar el objeto de la esa manera
+
+
+Vamos a crear una funcion llamada renderizarHistorial
+
+va a llamar al historial guardado y por cada elemento del historial vamos a crear la sig plantilla 
+`
+Accion: {accion}
+Operacion: {operacion}
+Numeros: {a}, {b}
+Resultado: {resultado}
+fecha: {fecha}
+`
+
+y la acumularan en un string y al finalizar el recorrido mostraran el string por alerta
+
+
+Para hacer el ejercicio recuerden previamente guardar algunas operaciones en el historial y borren las anteriores
+
+ */
+
+guardarEnHistorial({
+    accion: 'calculadora',
+    operacion: '+',
+    a: 3,
+    b: 3,
+    resultado: 6,
+    fecha:  '18|01/2020'
+})
+
+guardarEnHistorial({
+    accion: 'calculadora',
+    operacion: '-',
+    a: 6,
+    b: 3,
+    resultado: 3,
+    fecha:  '18|01/2020'
+})
+
+guardarEnHistorial({
+    accion: 'calculadora',
+    operacion: '+',
+    a: 4,
+    b: 3,
+    resultado: 7,
+    fecha:  '18|01/2020'
+})
+
+
+const renderizarHistorial = () =>{
+    const historial = obtenerHistorial()
+    let listaHistorial = ''
+    for (let obj of historial){
+        listaHistorial = listaHistorial + `
+        Accion: ${obj.accion}
+        Operacion: ${obj.operacion}
+        Numeros:${obj.a} ${obj.b}
+        Resultado: ${obj.resultado}
+        `
+    }
+    return listaHistorial
+}
+alert(renderizarHistorial)
+
