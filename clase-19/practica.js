911eeb5323b159ad6660f80eb9d10602b0584a95
mariana.urzainqui <marianaurzainqui@gmail.com>
2024-06-24 12:07:17 -0300

    tarea

--- clase-19/practica.js
+++ clase-19/practica.js
@@ -10,7 +10,10 @@ Una vez obtenido el JSON de esa direccion deberas renderizar todos los posts en 
 </div>
 */
 
-const postContainer = document.getElementById('posts-container')
+const postContainerHTML = document.getElementById('posts-container')
+const formHTML = document.getElementById('post-form')
+const titleHTML = document.getElementById('title')
+const postHTML = document.getElementById('post')
 
 const solicitarPost = async () =>{
     const respuesta = await fetch('https://jsonplaceholder.typicode.com/posts',{
@@ -30,6 +33,37 @@ const solicitarPost = async () =>{
         </div>
             `
     }
-    postContainer.innerHTML = html
+    postContainerHTML.innerHTML = html
 }
 solicitarPost()
+
+/* 
+Crear un formulario de posteo
+el formulario tendra un input para el titulo 
+y un textarea para el body y un boton con el texto post que active el evento submit
+
+Cuando se envie el formulario se debera capturar titulo y body y se envia via fetch a esta direccion: 
+'https://jsonplaceholder.typicode.com/posts'
+Y van a mostrar por consola un 'Enviado'
+*/
+
+const postObject = (title,body) => {
+    return {
+        title: title,
+        body: body,
+    }
+}
+
+const sendPost = (postObject) =>{
+    return fetch('https://jsonplaceholder.typicode.com/posts',{
+    method: 'POST',
+    body: JSON.stringify(postObject)
+})
+}
+
+const handleSubmit = async (event) =>{
+    event.preventDefault()
+    console.log('formulario enviado')
+}
+
+formHTML.addEventListener('submit', handleSubmit)
